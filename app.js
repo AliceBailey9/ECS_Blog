b219fbef447ee5a42cb2a344852bb42cde68de36
Alice <alice.bailey@ecs.co.uk>
2021-10-26 10:40:32 +0100

    nav bar

--- app.js
+++ app.js
@@ -16,13 +16,23 @@ app.engine(
   })
 );
 
+const blogs = ["blog1", "blog2", "blog3", "blog4"];
+
 //Serves static files (we need it to import a css file)
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-  res.render("main", { layout: "index", stuff: "this is stuff" });
+  res.render("main", { layout: "index", blogs: blogs });
   //res.render("main");
+});
+
+app.get("/postblog", (req, res) => {
+  res.render("postblog", { layout: "index" });
+});
+
+app.get("/allblogs", (req, res) => {
+  res.render("allblogs", { layout: "index" });
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
