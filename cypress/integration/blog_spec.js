8379efcc4ff4f24c1ae53c291c1af89ba0254519
Alice <alice.bailey@ecs.co.uk>
2021-10-28 14:22:03 +0100

    more cypress tests

--- cypress/integration/blog_spec.js
+++ cypress/integration/blog_spec.js
@@ -1,4 +1,4 @@
-describe("My First Test", () => {
+describe("Blog site tesing", () => {
   beforeEach(() => {
     cy.visit("localhost:9090");
   });
@@ -11,5 +11,24 @@ describe("My First Test", () => {
   it("clicking on home in Nav bar takes you to home page with all blogs", () => {
     cy.get(":nth-child(1) > .nav-link").click();
     cy.url().should("include", "/allblogs");
+  });
+  it("clicking on upload a blog in Nav bar takes you to post blog page", () => {
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.url().should("include", "/postblog");
+  });
+  it("home page contains title of recent blogs", () => {
+    cy.get(":nth-child(1) > .nav-link").click();
+    cy.get("h1").should("have.text", "Recent Blogs");
+  });
+  it("post blog form contains area to submit", () => {
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.get("form").contains("Blog content");
+    cy.get("form").contains("Blog title");
+    cy.get("form").contains("Submit").click();
+  });
+  it("submitting form takes you back to welcome page", () => {
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.get("form").contains("Submit").click();
+    cy.url().should("include", "/");
   });
 });
